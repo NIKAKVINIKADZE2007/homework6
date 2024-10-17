ea56b9b02f71ab622ec9064e36bf0232b6ba5b6d
nikakvinikadze <nikakvinikadze2007@gmail.com>
2024-10-17 23:45:46 +0400

    adding header

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className='mx-auto max-w-[625px] w-full text-center my-[67px]'>
+      <h1 className='text-White font-extrabold text-[81.85px]'>
+        Get in <span className='text-gradient '>touch</span>
+      </h1>
+      <p className='text-White text-[22px] font-medium opacity-70 '>
+        Reach out, and let's create a universe of possibilities together!
+      </p>
+    </header>
+  );
+};
+
+export default Header;
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'text-gradient': 'linear-gradient(to right, #272727 0%, #11101D 100%)',
+      },
+      colors: {
+        primary: '#0A0D17',
+        fontPrimary: '#763AF5',
+        fontSecondary: '#A604F2',
+        White: '#ffffff',
+      },
+    },
+  },
+  plugins: [],
+};
